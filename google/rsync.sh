14ef501ea74bc154a693de20b514ba49cea926e9
Qvazar <sebjorg@gmail.com>
2016-02-03 14:29:12 +0100

    initial setup

--- google/rsync.sh
+++ google/rsync.sh
@@ -3,6 +3,8 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-gsutil -m rsync -c -d -r ${DIR}/../www gs://www.invokit.dk/
+cd ${DIR}/.. && npm run build
+
+gsutil -m rsync -c -d -r ${DIR}/../www/build gs://www.invokit.dk/
 
 gsutil -m acl ch -r -u AllUsers:R gs://www.invokit.dk/
